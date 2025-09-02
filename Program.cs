dec2fb5493a1a613932c021be2c218a008f53a32
Felipe Dudek <felipe.dudek@outlook.com>
2025-09-02 16:19:34 -0300

    New features applied

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("99998888", "Tojolão", "123456", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Gmail");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("11112222", "Esquenta muito", "654321", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Apple Music");
